1c99c9d5dedf0bb562d1ba9522b5005d1d64d71a
徐4四 <xujing@cai-inc.com>
2019-07-03 15:42:22 +0800

    新增树工具

--- src/main/java/com/xjdy/common/tree/TreeUtil.java
+++ src/main/java/com/xjdy/common/tree/TreeUtil.java
@@ -1,12 +1,12 @@
 package com.xjdy.common.tree;
 
 import com.google.common.collect.Lists;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.ToString;
 import org.apache.commons.collections4.CollectionUtils;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -117,28 +117,5 @@ public class TreeUtil {
         });
 
         return result;
-    }
-
-    public static void main(String[] args) {
-        List<Tree> trees = new ArrayList<Tree>(){{
-           add(new Tree("1",null,"jim",new ArrayList<>()));
-           add(new Tree("2","1","tom",new ArrayList<>()));
-           add(new Tree("3",null,"jack",new ArrayList<>()));
-           add(new Tree("4","2","hello",new ArrayList<>()));
-           add(new Tree(null,"1","hello",new ArrayList<>()));
-        }};
-
-        List<Tree> treeList = convertToTreeCanFilter(trees,Tree::getCode,Tree::getParentCode,Tree::getChild,(Tree tree) -> tree.getName().equals("hello"));
-        System.out.println(treeList);
-    }
-
-    @Data
-    @AllArgsConstructor
-    @ToString
-    static class Tree{
-        private String code;
-        private String parentCode;
-        private String name;
-        private List<Tree> child = new ArrayList<>();
     }
 }
---
+++ src/test/java/com/xjdy/common/tree/CommonTree.java
@@ -0,0 +1,40 @@
+package com.xjdy.common.tree;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.xjdy.common.tree.TreeUtil.convertToTreeCanFilter;
+
+/**
+ * @author ：xusi
+ * @date ：Created in 2019-07-03 15:41
+ */
+public class CommonTree {
+
+    public static void main(String[] args) {
+        List<Tree> trees = new ArrayList<Tree>(){{
+            add(new Tree("1",null,"jim",new ArrayList<>()));
+            add(new Tree("2","1","tom",new ArrayList<>()));
+            add(new Tree("3",null,"jack",new ArrayList<>()));
+            add(new Tree("4","2","hello",new ArrayList<>()));
+            add(new Tree(null,"1","hello",new ArrayList<>()));
+        }};
+
+        List<Tree> treeList = convertToTreeCanFilter(trees,Tree::getCode,Tree::getParentCode,Tree::getChild,(Tree tree) -> tree.getName().equals("hello"));
+        System.out.println(treeList);
+    }
+
+    @Data
+    @AllArgsConstructor
+    @ToString
+    static class Tree{
+        private String code;
+        private String parentCode;
+        private String name;
+        private List<Tree> child = new ArrayList<>();
+    }
+}
